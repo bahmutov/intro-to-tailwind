51a49531fda48f0e3de962eddfa8f22ec4feb37e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-12-22 07:29:17 -0500

    good layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   content: ['src/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', 'sans-serif'],
+        title: ['Cursive', 'sans-serif'],
+      },
+      colors: {
+        gray: {
+          50: '#fafafa',
+          100: '#f5f5f5',
+          200: '#eeeeee',
+          300: '#e0e0e0',
+          400: '#bdbdbd',
+          500: '#9e9e9e',
+          600: '#757575',
+          700: '#616161',
+          800: '#2f3136',
+          900: '#202225',
+        },
+      },
+    },
   },
   plugins: [],
 }
